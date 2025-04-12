e6147fc3bb885c519ce74e5fef562e7e8c9d92dd
NethmiPramoddya <nethmik2001@gmail.com>
2025-04-13 01:39:30 +0530

    Basic_request_routing_setup

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3500
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use(express.json())
+
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/',require('./routes/root'))
+
+
+
 
 app.listen(PORT,()=>console.log(`Server run on port ${PORT}`))
